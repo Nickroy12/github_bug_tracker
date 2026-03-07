033d2d6132d86d01eea2e936661d8eae2de9bf18
Nick-roy <nroy08232@gmail.com>
2026-03-07 11:46:35 +0600

    Login page Complete

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const loginBtn = document.getElementById('loginSubmit');
+console.log(loginBtn)
+
+loginBtn.addEventListener('click', function(){
+   const nameInput = document.getElementById('nameInput');
+const name = nameInput.value.trim();
+if(name === ""){
+   return alert('Input user name')
+}
+const passIn = document.getElementById('password');
+const pass = passIn.value.trim();
+if(pass === ''){
+    return alert('Input user Password')
+}
+
+if(name === "Nick Roy" && pass ==="admin1234"){
+   alert('Your signing Successful  !')
+   window.location.assign('/dashboard.html')
+}else{
+   alert('Something is wrong !')
+}
+
+})
