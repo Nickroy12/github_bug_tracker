1de492d4d59abe1d6d8d00ce839b9b8180f7574a
Nick-roy <nroy08232@gmail.com>
2026-03-08 12:09:03 +0600

    50% done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,80 @@
 const issueCount = document.getElementById('issueCount');
 const issueContainer = document.getElementById('issuContainer');
+
+const loader = (status) =>{
+    if(status === true){
+        document.getElementById('spinner').classList.remove('hidden');
+        issueContainer.classList.add('hidden' )
+    }else{
+          issueContainer.classList.remove('hidden')
+           document.getElementById('spinner').classList.add('hidden' );
+    }
+
+}
 async function dataFetch(){
-   try{
-      const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
-      const data = await res.json();
-      dataDisplay(data.data);
-   }
-   catch(error){
-      console.log("Error:", error);
-   }
+  try{
+    loader(true)
+    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+    const data = await res.json();
+    dataDisplay(data.data);
+  }
+  catch(error){
+    console.log("Error:", error)
+  }
+  finally{
+    loader(false)
+  }
+}
+async function modalBox(id){
+  const res = await  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+  const data = await res.json()
+  displayDetails(data.data)
 }
 
-dataFetch();
+const displayDetails = (issue) =>{
+
+  const modalContainer = document.getElementById('modalBody');
+  // modalContainer.innerHTML = `hi nick`
+
+  
+      modalContainer.innerHTML = `
+        <div class="title">
+       <h2 class="card-title text-2xl font-bold ">${issue.title}</h2>
+        </div>
+        <div class="intro flex gap-2 ">
+           <span class="rounded-3xl bg-green-500  px-2 text-white font-semibold  ${issue.status === "open" ? 'bg-green-500 rounded-3xl': 'bg-red-500 rounded-3xl'} ">${issue.status}</span>
+           <span class="rounded-3xl to-slate-400 font-semibold ">• ${issue.status} by ${issue.author} </span>
+           <span class="rounded-3xl  to-slate-400 font-semibold">• ${new Date(issue.createdAt).toLocaleDateString()}</span>
+        </div>
+      <div class="card-actions ">
+      <span class="btn btn-error rounded-3xl btn-soft"><i class="fa-solid fa-bug"></i>${issue.labels[0]}</span>
+      <button class="btn btn-warning rounded-3xl btn-soft ${issue.labels[1] === undefined? 'hidden':'flex'}">${issue.labels[1]}</button>
+    </div>
+     <p class="text-slate-500">${issue.description}</p>
+     <div class="workDess flex justify-between items-center ">
+      <div class="assign">
+        <h2 class="text-slate-500">Assignee:</h2>
+        <p class="font-bold"> ${issue.assignee} </p>
+      </div>
+      <div class="priority">
+        <span class="rounded-3xl bg-green-500 px-2 text-white font-semibold ">${issue.priority} </span>
+      </div>
+     </div>`
+  
+  document.getElementById('popupbox').showModal();
+}
 
 const dataDisplay =(data) =>{
     console.log(data)
     issueCount.innerHTML = data.length
-    //  issueContainer.innerHTML = ' '
+     issueContainer.innerHTML = ' '
 
     data.forEach(e => {
         const div = document.createElement('div');
         div.className = '';
 
         div.innerHTML = `
-                  <div class="card bg-base-100  shadow-sm mb-3">
+                  <div onclick="modalBox(${e.id})" class="card bg-base-100  shadow-sm mb-3 cursor-pointer">
   <div class="card-body ${e.status === "open" ? 'border-b-2 border-green-500 rounded-3xl': 'border-b-2 border-red-500 rounded-3xl'}" >
     <div class="flex justify-between">
       <div class="git-status">
@@ -47,3 +98,4 @@ const dataDisplay =(data) =>{
        issueContainer.appendChild(div)
     });
 }
+dataFetch();
